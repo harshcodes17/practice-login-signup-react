f8a048d69d0c9a1995226599ba30e9086f08a890
harshcodes17 <harshabamane1718@gmail.com>
2024-03-22 19:38:05 +0530

    done with JS of login signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './components/LoginSignup/LoginSignup.jsx'
+import './components/HomePage/HomePage.jsx'
+import HomePage from './components/HomePage/HomePage.jsx';
 
 function App() {
   return (
     <div>
+      {/* <HomePage/> */}
       <LoginSignup/>
     </div>
+
     
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./HomePage.css";
+import user_icon from "../assets/person.png";
+import email_icon from "../assets/email.png";
+import password_icon from "../assets/password.png";
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+
+      <div className="container">
+        <div className="header">
+          <div className="text"> Sign Up </div>
+          <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email Id" />
+          </div>
+
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+
+        <div className="fogot-password">
+          Lost password? <span>Click here!</span>
+        </div>
+        <div className="submit-container">
+          <div className="submit"> Sign Up</div>
+          <div className="submit"> Login</div>
+        </div>
+      </div>
+
+      <div className="container2">
+        <div className="header">
+          <div className="text"> Sign Up </div>
+          <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email Id" />
+          </div>
+
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+
+        <div className="fogot-password">
+          Lost password? <span>Click here!</span>
+        </div>
+        <div className="submit-container">
+          <div className="submit"> Sign Up</div>
+          <div className="submit"> Login</div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default HomePage;
