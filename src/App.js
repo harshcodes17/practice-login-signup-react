f2ad6bc9a5c50f82077526ba513f462e277f3081
harshcodes17 <harshabamane1718@gmail.com>
2024-03-22 14:14:00 +0530

    3rd

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div>
       <LoginSignup/>
     </div>
+    
   );
 }
 
--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,42 +1,45 @@
-import React from 'react'
+import React from "react";
 // import './LoginSignUp.css'
-import './LoginSignup.css'
-import user_icon from '../assets/login.png'
-import email_icon from '../assets/envelope.png'
-import password_icon from '../assets/lock.png'
+import "./LoginSignup.css";
+import user_icon from "../assets/person.png";
+import email_icon from "../assets/email.png";
+import password_icon from "../assets/password.png";
 //import submit_icon from '../assets/security.png'
 const LoginSignup = () => {
   return (
-    < div className = 'container'>
-    <div className = "header">
-    <div className="text"> Sign up </div>
-            <div className="underline"></div>
-</div>
-    <div className = "inputs">
-    <div className ="input"></div>
-    <img src= {user_icon }alt=""/>
-    <input type ="text" placeholder='Name'/>
-    </div>
+    <div className="container">
+      <div className="header">
+        <div className="text"> Sign Up </div>
+        <div className="underline"></div>
+      </div>
 
-    <div className = "inputs">
-    <div className ="input"></div>
-    <img src={email_icon}alt=""/>
-    <input type ="email" placeholder='Email Id'/>
-    </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder="Name" />
+        </div>
 
-    <div className = "inputs">
-    <div className ="input"></div>
-    <img src={password_icon}alt=""/>
-    <input type ="password" placeholder='Password' />
-    </div>
-    <div className = "fogot password">Lost password? <span>Click here!</span>
-   <div className ="submit-container"></div>
-   <div className ="submit"> SignUp</div>
-   <div className ="submit"> Login</div>
-   
-   </div>
-   </div>
-  )
-}
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email Id" />
+        </div>
 
-export default LoginSignup
+        <div className="input">
+        <img src={password_icon} alt="" />
+        <input type="password" placeholder="Password" />
+        </div>
+
+      </div>
+
+      <div className="fogot-password">
+        Lost password? <span>Click here!</span>
+      </div>
+      <div className="submit-container">
+        <div className="submit"> Sign Up</div>
+        <div className="submit"> Login</div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
