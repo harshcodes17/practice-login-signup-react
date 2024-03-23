02a1967887992e97b86819bc072f82bde690d3d2
harshcodes17 <harshabamane1718@gmail.com>
2024-03-23 12:24:39 +0530

    added HomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,70 +8,71 @@ const HomePage = () => {
     <div>
       <h1>Navbar</h1>
 
-      <div className="container">
-        <div className="header">
-          <div className="text"> Sign Up </div>
-          <div className="underline"></div>
-        </div>
-
-        <div className="inputs">
-          <div className="input">
-            <img src={user_icon} alt="" />
-            <input type="text" placeholder="Name" />
+      <div className="parent-container">
+        <div className="container">
+          <div className="header">
+            <div className="text"> Sign Up </div>
+            <div className="underline"></div>
           </div>
 
-          <div className="input">
-            <img src={email_icon} alt="" />
-            <input type="email" placeholder="Email Id" />
+          <div className="inputs">
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+
+            <div className="input">
+              <img src={email_icon} alt="" />
+              <input type="email" placeholder="Email Id" />
+            </div>
+
+            <div className="input">
+              <img src={password_icon} alt="" />
+              <input type="password" placeholder="Password" />
+            </div>
           </div>
 
-          <div className="input">
-            <img src={password_icon} alt="" />
-            <input type="password" placeholder="Password" />
+          <div className="fogot-password">
+            Lost password? <span>Click here!</span>
+          </div>
+          <div className="submit-container">
+            <div className="submit"> Sign Up</div>
+            <div className="submit"> Login</div>
           </div>
         </div>
 
-        <div className="fogot-password">
-          Lost password? <span>Click here!</span>
-        </div>
-        <div className="submit-container">
-          <div className="submit"> Sign Up</div>
-          <div className="submit"> Login</div>
+        <div className="container2">
+          <div className="header">
+            <div className="text"> Sign Up </div>
+            <div className="underline"></div>
+          </div>
+
+          <div className="inputs">
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+
+            <div className="input">
+              <img src={email_icon} alt="" />
+              <input type="email" placeholder="Email Id" />
+            </div>
+
+            <div className="input">
+              <img src={password_icon} alt="" />
+              <input type="password" placeholder="Password" />
+            </div>
+          </div>
+
+          <div className="fogot-password">
+            Lost password? <span>Click here!</span>
+          </div>
+          <div className="submit-container">
+            <div className="submit"> Sign Up</div>
+            <div className="submit"> Login</div>
+          </div>
         </div>
       </div>
-
-      <div className="container2">
-        <div className="header">
-          <div className="text"> Sign Up </div>
-          <div className="underline"></div>
-        </div>
-
-        <div className="inputs">
-          <div className="input">
-            <img src={user_icon} alt="" />
-            <input type="text" placeholder="Name" />
-          </div>
-
-          <div className="input">
-            <img src={email_icon} alt="" />
-            <input type="email" placeholder="Email Id" />
-          </div>
-
-          <div className="input">
-            <img src={password_icon} alt="" />
-            <input type="password" placeholder="Password" />
-          </div>
-        </div>
-
-        <div className="fogot-password">
-          Lost password? <span>Click here!</span>
-        </div>
-        <div className="submit-container">
-          <div className="submit"> Sign Up</div>
-          <div className="submit"> Login</div>
-        </div>
-      </div>
-
     </div>
   );
 };
--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./LoginSignup.css";
 import user_icon from "../assets/person.png";
 import email_icon from "../assets/email.png";
@@ -8,33 +8,77 @@ const LoginSignup = () => {
   const [action, setAction] = useState("Sign Up");
 
   return (
-    <div className="container">
-      <div className="header">
-        <div className="text"> {action} </div>
-        <div className="underline"></div>
-      </div>
+    <div className="main_container">
+      <div className="container">
+        <div className="header">
+          <div className="text"> {action} </div>
+          <div className="underline"></div>
+        </div>
 
-      <div className="inputs">
-        {action ==="Login"?<div></div>:<div className="input">
-            <img src={user_icon} alt="" />
-            <input type="text" placeholder="Name" />
-            </div>}
+        <div className="inputs">
+          {action === "Login" ? (
+            <div></div>
+          ) : (
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+          )}
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email Id" />
+          </div>
+
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+        {action === "Sign Up" ? (
+          <div></div>
+        ) : (
+          <div className="fogot-password">
+            Lost password? <span>Click here!</span>
+          </div>
+        )}
         
-        <div className="input">
-          <img src={email_icon} alt="" />
-          <input type="email" placeholder="Email Id" />
-        </div>
-
-        <div className="input">
-        <img src={password_icon} alt="" />
-        <input type="password" placeholder="Password" />
-        </div>
-
       </div>
-    {action ==="Sign Up"? <div></div>: <div className="fogot-password">Lost password? <span>Click here!</span></div>}
-      <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+
+      <div className="container">
+        <div className="header">
+          <div className="text"> {action} </div>
+          <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+          {action === "Login" ? (
+            <div></div>
+          ) : (
+            <div className="input">
+              <img src={user_icon} alt="" />
+              <input type="text" placeholder="Name" />
+            </div>
+          )}
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email Id" />
+          </div>
+
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+        {action === "Sign Up" ? (
+          <div></div>
+        ) : (
+          <div className="fogot-password">
+            Lost password? <span>Click here!</span>
+          </div>
+        )}
+        
       </div>
     </div>
   );
